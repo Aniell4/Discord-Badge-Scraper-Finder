4b42f58f6abf4211cd3823c691d78a2837eff13f
Gumi <clashroyaleixo@gmail.com>
2023-04-19 16:30:40 +0200

    fix it for u lil bro

--- main.py
+++ main.py
@@ -3,42 +3,45 @@ import asyncio
 
 
 def check_flags(flag_int):
-	################
-	flag_dict = {
-		'Discord_Employee': 1,
-		'Partnered_Server_Owner': 2,
-		'HypeSquad_Events': 4,
-		'Bug_Hunter_Level_1': 8,
-		#'House_Bravery': 64,
-		#'House_Brilliance': 128,
-		#'House_Balance': 256,
-		'Early_Supporter': 512,
-		'Team_Pseudo_user': 1024,
-		'Bug_Hunter_Level_2': 16384,
-		'Verified_Bot': 65536,
-		'Early_Verified_Bot_Developer': 131072,
-		'Discord_Certified_moderator': 262144,
-		'BOT_HTTP_INTERACIONS': 524288,
-	}
-	################
+    flag_dict = {
+        "Discord_Employee": 1,
+        "Partnered_Server_Owner": 2,
+        "HypeSquad_Events": 4,
+        "Bug_Hunter_Level_1": 8,
+        "Early_Supporter": 512,
+        "Team_Pseudo_user": 1024,
+        "Bug_Hunter_Level_2": 16384,
+        "Verified_Bot": 65536,
+        "Early_Verified_Bot_Developer": 131072,
+        "Discord_Certified_moderator": 262144,
+        "BOT_HTTP_INTERACIONS": 524288,
+    }
 
-	flags = []
-	for flag_key in flag_dict.keys():
-		if ((flag_int & flag_dict[flag_key]) == flag_dict[flag_key]):
-			flags.append(flag_key)
+    # fixed it for u lil bro
+    flags = []
+    for flag_key, flag_value in flag_dict.items():
+        if flag_int & flag_value:
+            flags.append(flag_key)
 
-	return flags
+    return flags
+
 
 async def main():
-	members = scrape(input('[SCRAPER] Token to Scrape with: '), input('[SCRAPER] Guild ID to Scrape: '), input('[SCRAPER] Channel ID to Scrape: '))
-	with open('hits.txt', 'a') as f:
-		for key in members.keys():
-			try:
-				flags = check_flags(members[key]['public_flags'])
-				if flags != []:
-					f.write(key + ' - ' + ''.join(flag + ', ' for flag in flags) + '\n')
-			except : pass	
+    members = scrape(
+        input("[SCRAPER] Token to Scrape with: "),
+        input("[SCRAPER] Guild ID to Scrape: "),
+        input("[SCRAPER] Channel ID to Scrape: "),
+    )
+    with open("hits.txt", "a") as f:
+        for key in members.keys():
+            try:
+                flags = check_flags(members[key]["public_flags"])
+                if flags != []:
+                    f.write(key + " - " + "".join(flag + ", " for flag in flags) + "\n")
+            except:
+                pass
 
-if __name__ == '__main__':
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(main())
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
